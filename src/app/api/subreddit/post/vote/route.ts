c1b1f0c06c9fa27f10efac1317bb63fb8133fd63
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-15 19:28:48 +0530

    commited

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -5,13 +5,13 @@ import { PostVoteValidator } from '@/lib/validators/vote'
 import { CachedPost } from '@/types/redis'
 import { z } from 'zod'
 
-const CACHE_AFTER_UPVOTES = 1
+const CACHE_AFTER_UPVOTES = 1;
 
 export async function PATCH(req: Request) {
   try {
     const body = await req.json()
 
-    const { postId, voteType } =   PostVoteValidator.parse(body)
+    const { postId, voteType } =   await PostVoteValidator.parse(body)
 
     const session = await getAuthSession()
 
@@ -20,7 +20,7 @@ export async function PATCH(req: Request) {
     }
 
     // check if user has already voted on this post
-    const existingVote = await db.vote.findFirst({
+    const existingVote = await db.vote?.findFirst({
       where: {
         userId: session.user.id,
         postId,
@@ -77,7 +77,7 @@ export async function PATCH(req: Request) {
       }
 
       // if vote type is different, update the vote
-      await db.vote.update({
+      await db.vote?.update({
         where: {
           userId_postId: {
             postId,
@@ -140,12 +140,13 @@ export async function PATCH(req: Request) {
       }
 
      const x =  await redis.hset(`post:${postId}`, cachePayload) // Store the post data as a hash
-     console.log(x,"X")
+    
     }
 
     return new Response('OK')
   } catch (error) {
     (error)
+     console.log(error,"XERRRRRRRRRRRRRRRRROend")
     if (error instanceof z.ZodError) {
       return new Response(error.message, { status: 400 })
     }
