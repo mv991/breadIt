79c7f2c5eebe8dede253465041cc4986246586db
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-16 11:28:27 +0530

    commited

--- src/hooks/use-custom-toasts.tsx
+++ src/hooks/use-custom-toasts.tsx
@@ -1,23 +1,25 @@
-import { buttonVariants } from '@/components/ui/Button'
-import { toast } from '@/hooks/use-toast'
-import Link from 'next/link'
+import { buttonVariants } from "@/components/ui/Button";
+import { toast } from "@/hooks/use-toast";
+import Link from "next/link";
 
 export const useCustomToasts = () => {
   const loginToast = () => {
     const { dismiss } = toast({
-      title: 'Login required.',
-      description: 'You need to be logged in to do that.',
-      variant: 'destructive',
+      title: "Login required.",
+      description: "You need to be logged in to do that.",
+      variant: "destructive",
       action: (
         <Link
           onClick={() => dismiss()}
-          href='/sign-in'
-          className={buttonVariants({ variant: 'outline' })}>
+          href="/sign-in"
+          className={buttonVariants({ variant: "outline" })}
+        >
           Login
         </Link>
       ),
-    })
-  }
+    });
+  };
+  // changed somehitnf
 
-  return { loginToast }
-}
+  return { loginToast };
+};
