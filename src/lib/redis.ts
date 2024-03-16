a50d640d4c694315ccd601f279fefd657437e49d
Mahak Vaid <mahakvaid1999@gmail.com>
2024-03-16 09:44:04 +0530

    redis changes

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,6 @@
 import { Redis } from '@upstash/redis'
-import "server-only"
+
 export const redis = new Redis({
   url: process.env.REDIS_URL!,
   token: process.env.REDIS_SECRET!,
 })
-// console.log(process.env.REDIS_URL,process.env.REDIS_SECRET,"redis",redis)
